f4e7af45c3d860b34d30a0ba752f7a2dc20ed001
Balázs Gunics <gunicsba@gmail.com>
2025-07-12 18:58:51 +0200

    Lorantnak

--- TeensyModules/V4.1/Firmware/Autosteer_gps_teensy_v4_1/Autosteer.ino
+++ TeensyModules/V4.1/Firmware/Autosteer_gps_teensy_v4_1/Autosteer.ino
@@ -43,7 +43,7 @@
 //Define sensor pin for current or pressure sensor
 #define CURRENT_SENSOR_PIN A17
 #define PRESSURE_SENSOR_PIN A10
-#define JOHNDEERE false
+#define JOHNDEERE true
 elapsedMicros dutyTime = 0;
 float dutyTimeCurrent = 0;
 float dutyTimePrev = 0;
@@ -182,7 +182,6 @@ void ISRJOHNDEERERISING(){
 }
 
 void ISRJOHNDEEREFALLING(){
-  if(dutyTime  < 50) return;
   attachInterrupt(digitalPinToInterrupt(PRESSURE_SENSOR_PIN), ISRJOHNDEERERISING, RISING);
   dutyTimeCurrent = dutyTime;
   return;
