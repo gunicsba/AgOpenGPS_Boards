ff9b08b578251006b53bc7348548e82cde1d1bec
Gunics Balázs <gunicsba@gmail.com>
2026-03-03 18:56:49 +0100

    Gojszanak...

--- TeensyModules/V4.1/Firmware/Autosteer_gps_teensy_v4_1/Autosteer_gps_teensy_v4_1.ino
+++ TeensyModules/V4.1/Firmware/Autosteer_gps_teensy_v4_1/Autosteer_gps_teensy_v4_1.ino
@@ -29,7 +29,7 @@
 // Serial Ports
 #define SerialAOG Serial                //AgIO USB conection
 #define SerialRTK Serial3               //RTK radio
-HardwareSerial* SerialGPS = &Serial5; //7  //Main postion receiver (GGA) (Serial2 must be used here with T4.0 / Basic Panda boards - Should auto swap)
+HardwareSerial* SerialGPS = &Serial7; //7  //Main postion receiver (GGA) (Serial2 must be used here with T4.0 / Basic Panda boards - Should auto swap)
 HardwareSerial* SerialGPS2 = &Serial2;  //2 Dual heading receiver 
 HardwareSerial* SerialGPSTmp = NULL;
 //HardwareSerial* SerialAOG = &Serial;
